f61fb699989ad5dc6455053978a1d13bca990fce
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-10-06 14:23:02 -0300

    Create project

---
+++ src/main/java/com/webapp/WebApp/model/Task.java
@@ -0,0 +1,42 @@
+package com.webapp.WebApp.model;
+
+import java.util.Date;
+
+public class Task {
+    private Long id;
+    private String name;
+    private Date date;
+
+    public Task (){
+    }
+
+    public Task(Long id, String name, Date date) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
